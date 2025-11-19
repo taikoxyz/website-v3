c02418abc10fd9358c85490725e9f7d018812634
Myron Rotter <myronrotter01@t-online.de>
2025-11-19 10:52:31 +0100

    chore: change mirror to paragraph

--- src/widgets/old/footer/ui/footer-socials/index.tsx
+++ src/widgets/old/footer/ui/footer-socials/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useTranslationObject } from 'shared/lib/hooks/use-translation-object';
+import Image from 'shared/ui/image';
 import Sprite from 'shared/ui/sprite';
 import { IFooterSocial } from 'widgets/old/footer/lib/types';
 import css from './footer-socials.module.scss';
-import Image from 'shared/ui/image';
 
 export const FooterSocials: React.FC = () => {
     const socials = useTranslationObject<IFooterSocial[]>('socials', 'footer');
@@ -19,9 +19,9 @@ export const FooterSocials: React.FC = () => {
                 >
                     {social.name}
                     <div className={css.socials_wrapper}>
-                        {social.name === "mirror" ? (
+                        {social.name === "paragraph" ? (
                             <div className={css.mirror_logo}>
-                                <Image.Default src="/img/mirror-logo.png" alt="" />
+                                <Image.Default src="/img/paragraph-logo.png" alt="" />
                             </div>
                         ) : (
                             <Sprite.Default icon={social.icon} />
